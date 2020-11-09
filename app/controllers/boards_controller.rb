2df5c937df1693a17b754e3a714c3054210713b2
Eddie Kao <eddie@5xruby.tw>
2020-11-09 16:59:34 +0800

    create board

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -3,9 +3,18 @@ class BoardsController < ApplicationController
   end
 
   def new
+    @board = Board.new
   end
 
   def create
-    redirect_to "/"
+    clean_params = params.require(:board).permit(:title)
+    @board = Board.new(clean_params)
+
+    if @board.save
+      # flash["notice"] = "成功新增看板"
+      redirect_to "/", notice: '成功新增看板'
+    else
+      render :new
+    end
   end
 end
