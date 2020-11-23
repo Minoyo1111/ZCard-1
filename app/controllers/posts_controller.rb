4db886ac477988e814723c90c99f4871e7eda778
Eddie Kao <eddie@5xruby.tw>
2020-11-23 17:01:19 +0800

    update posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,10 @@
 class PostsController < ApplicationController
-  before_action :session_required, only: [:create]
+  before_action :session_required, only: [:create, :edit, :update]
   before_action :set_board, only: [:new, :create]
+  before_action :set_post, only: [:show]
+
+  def show
+  end
 
   def new
     @post = Post.new
@@ -24,6 +28,21 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    # @post = Post.find_by!(id: params[:id], user: current_user)
+    @post = current_user.posts.find(params[:id])
+  end
+
+  def update
+    @post = current_user.posts.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to @post, notice: '文章更新成功'
+    else
+      render :edit
+    end
+  end
+
   private
   def set_board
     @board = Board.find(params[:board_id])
@@ -31,5 +50,9 @@ class PostsController < ApplicationController
 
   def post_params
     params.require(:post).permit(:title, :content)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,8 @@ class Post < ApplicationRecord
 
   validates :title, presence: true
   validates :content, presence: true
+
+  def owned_by?(user)
+    self.user == user
+  end
 end
