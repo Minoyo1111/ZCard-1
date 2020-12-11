1c6668b53f0062a4ccd47aa40f53435a43dcddd6
Eddie Kao <eddie@5xruby.tw>
2020-12-11 10:46:11 +0800

    add script

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -11,3 +11,4 @@ require("scripts")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+import "controllers"
--- app/frontend/scripts/index.js
+++ app/frontend/scripts/index.js
@@ -1,1 +1,1 @@
-require('./post')
+// require('./post')
